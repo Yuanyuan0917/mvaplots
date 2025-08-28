9f5962333d3ca19605a711d242c64ac362ce571e
Yuanyuan Qu <quy@cip.ifi.lmu.de>
2025-08-28 14:11:27 +0200

    upda

--- R/heatmap_strips.R
+++ R/heatmap_strips.R
@@ -1,24 +1,14 @@
 #' Generate heatmap strips for multiverse outcomes
 #'
-#' Creates a set of heatmap strip plots that visualize how outcome estimates
-#' vary depending on analytical decision variables in a multiverse analysis.
-#'
 #' @param data A dataframe with results of the multiverse analysis.
 #' @param outcome_var Character. Name of the outcome variable.
 #' @param strip_vars Character vector. Names of decision variables to display.
 #' @param variable_labels Named vector. Display labels for the decision variables.
 #'
 #' @return A list of ggplot objects representing heatmap strips.
-#'
-#' @examples
-#' prep <- prep_data("hurricane")
-#' df <- prep[[1]]
-#' strip_vars <- prep[[4]]
-#' variable_labels <- prep[[5]]
-#' generate_heatmap_strips(df, "edif", strip_vars, variable_labels)
-#'
 #' @export
 generate_heatmap_strips <- function(data, outcome_var, strip_vars, variable_labels) {
+  # --- Binning setup ---
   n_bins <- 20
   x_min <- min(data[[outcome_var]], na.rm = TRUE)
   x_max <- max(data[[outcome_var]], na.rm = TRUE)
@@ -26,7 +16,7 @@ generate_heatmap_strips <- function(data, outcome_var, strip_vars, variable_labe
   common_xlim <- c(x_min, x_max + x_buffer)
   breaks <- seq(x_min, x_max, length.out = n_bins + 1)
 
-  # Fit linear model for reordering factor levels
+  # --- Fit linear model ---
   reg_formula <- as.formula(paste(outcome_var, "~", paste(strip_vars, collapse = " + ")))
   lm_model <- lm(reg_formula, data = data)
   coefs <- coef(lm_model)
@@ -40,7 +30,7 @@ generate_heatmap_strips <- function(data, outcome_var, strip_vars, variable_labe
     ) %>%
     dplyr::ungroup()
 
-  # Reorder factor levels
+  # --- Reorder factor levels ---
   for (var in strip_vars) {
     ref_level <- levels(data[[var]])[1]
     coef_sub <- tidy_coefs %>% dplyr::filter(var == !!var & level != ref_level) %>%
@@ -48,7 +38,7 @@ generate_heatmap_strips <- function(data, outcome_var, strip_vars, variable_labe
     data[[var]] <- factor(data[[var]], levels = unique(c(ref_level, coef_sub)))
   }
 
-  # Label lookup for coefficients
+  # --- Label lookup ---
   label_lookup <- tidy_coefs %>%
     dplyr::mutate(
       rounded = round(estimate, 2),
@@ -67,7 +57,7 @@ generate_heatmap_strips <- function(data, outcome_var, strip_vars, variable_labe
 
   label_lookup <- dplyr::bind_rows(label_lookup, ref_labels)
 
-  # Order variables by influence
+  # --- Order variables by influence ---
   influence_scores <- tidy_coefs %>%
     dplyr::group_by(var) %>%
     dplyr::summarise(max_abs_effect = max(abs(estimate), na.rm = TRUE)) %>%
@@ -76,17 +66,22 @@ generate_heatmap_strips <- function(data, outcome_var, strip_vars, variable_labe
   strip_vars <- influence_scores$var
   base_colors <- viridis::viridis(length(strip_vars), option = "D")
 
-  # Generate strips
+  # --- Generate strips ---
   heatmap_strips <- Map(function(varname, base_color) {
     var_label <- variable_labels[[varname]]
-    generate_heatmap_strip(data, varname, outcome_var, breaks, base_color, var_label, label_lookup, common_xlim, x_min, x_max)
+    generate_heatmap_strip(data, varname, outcome_var,
+                           breaks, base_color, var_label,
+                           label_lookup, common_xlim, x_min, x_max)
   }, varname = strip_vars, base_color = base_colors)
 
   return(heatmap_strips)
 }
 
+
 #' @keywords internal
-generate_heatmap_strip <- function(df, varname, outcome_var, breaks, base_color, var_label, label_lookup, common_xlim, x_min, x_max) {
+generate_heatmap_strip <- function(df, varname, outcome_var,
+                                   breaks, base_color, var_label,
+                                   label_lookup, common_xlim, x_min, x_max) {
   var_sym <- rlang::sym(varname)
   out_sym <- rlang::sym(outcome_var)
   x_label_pos <- x_max + 0.01 * (x_max - x_min)
@@ -108,7 +103,7 @@ generate_heatmap_strip <- function(df, varname, outcome_var, breaks, base_color,
   heatmap_data <- add_variable_label_row(heatmap_data, varname, base_color, var_label, x_min, x_max)
 
   label_data <- heatmap_data %>%
-    dplyr::filter(!is.na(!!var_sym), !is.na(prop)) %>%
+    dplyr::filter(!is.na(!!var_sym), !is.na(prop), !!var_sym != "label") %>%
     dplyr::distinct(!!var_sym) %>%
     dplyr::mutate(var = varname) %>%
     dplyr::rename(level_name = !!var_sym) %>%
@@ -120,18 +115,14 @@ generate_heatmap_strip <- function(df, varname, outcome_var, breaks, base_color,
 
   ref_level <- levels(df[[varname]])[1]
 
-  ggplot2::ggplot(heatmap_data, ggplot2::aes(y = !!var_sym, fill = prop)) +
-    ggplot2::geom_rect(
-      ggplot2::aes(xmin = xmin, xmax = xmax,
-                   ymin = as.numeric(!!var_sym) - 0.5,
-                   ymax = as.numeric(!!var_sym) + 0.5),
-      color = NA
-    ) +
+  ggplot2::ggplot(heatmap_data, ggplot2::aes(x = (xmin + xmax)/2, y = !!var_sym, fill = prop)) +
+    ggplot2::geom_tile(ggplot2::aes(width = xmax - xmin, height = 1), color = NA) +
     ggplot2::geom_text(data = label_data,
                        ggplot2::aes(y = !!var_sym, label = label_text),
-                       x = x_label_pos, inherit.aes = FALSE, hjust = 0, size = 3, parse = TRUE, na.rm = TRUE) +
+                       x = x_label_pos, inherit.aes = FALSE,
+                       hjust = 0, size = 3, parse = TRUE, na.rm = TRUE) +
     ggplot2::scale_fill_gradient(low = "white", high = base_color, na.value = NA) +
-    ggplot2::scale_x_continuous(limits = common_xlim, expand = c(0, 0)) +
+    ggplot2::scale_x_continuous(limits = c(x_min, x_max), expand = c(0, 0)) +
     ggplot2::scale_y_discrete(
       labels = function(labs) {
         ifelse(
@@ -157,7 +148,7 @@ generate_heatmap_strip <- function(df, varname, outcome_var, breaks, base_color,
 #' @keywords internal
 add_variable_label_row <- function(data, varname, base_color, var_label, x_min, x_max) {
   orig_levels <- levels(data[[varname]])
-  new_levels <- rev(c("label", orig_levels))
+  new_levels <- c("label", orig_levels)   # 保持原顺序，不要 rev()
   data[[varname]] <- factor(data[[varname]], levels = new_levels)
 
   label_row <- data.frame(
@@ -166,8 +157,8 @@ add_variable_label_row <- function(data, varname, base_color, var_label, x_min, 
     xmin = x_min,
     xmax = x_max
   )
-
   label_row[[varname]] <- factor("label", levels = new_levels)
+
   for (col in setdiff(names(data), names(label_row))) label_row[[col]] <- NA
   dplyr::bind_rows(label_row[, names(data)], data)
 }
